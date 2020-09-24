7b8dd46d7965c863e7085233aba30398dc6329c7
liviusceastlivii <70839080+liviusceastlivii@users.noreply.github.com>
2020-09-24 16:32:31 +0300

    Add files via upload

---
+++ operatori matematici 2.py
@@ -0,0 +1,13 @@
+#problema b
+print((4**5)*(2**13)/(8**7)+((27**4)*(9**5))/(3**20))
+#problema c
+print((5**103)/(((5**17)*(5**8))**4)+(324/(9**2)+5*(2**3))+(10**0))
+#problema d
+print((((2**5)**10)+(2**15)*(2**35)+(3**80)/(3**19))/((3**60)+((3**30)**2)+((3**3)**20)+(2**70)/(2**19)))
+#problema e
+import math
+print(2*math.sqrt(3+math.sqrt(5-math.sqrt(13+4*math.sqrt(3)))))
+#problema f
+print(math.sqrt(13+30*math.sqrt(2+math.sqrt(9+4*math.sqrt(2)))))
+#problema g
+print(math.sqrt(26+6*math.sqrt(13-4*math.sqrt(8+2*math.sqrt(6-2*math.sqrt(5)))))+math.sqrt(26-6*math.sqrt(13+4*math.sqrt(8-2*math.sqrt(6+2*math.sqrt(5))))))
